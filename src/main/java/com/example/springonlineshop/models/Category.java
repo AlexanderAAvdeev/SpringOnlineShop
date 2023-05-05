2802cb61cdeab904b2d5856a6cabd3c6795125c2
AlexanderAAvdeev <2035-a.a.avdeev@mpt.ru>
2023-05-05 21:36:42 +0300

    Authentication issue has been solved. Added a new model Category, Product, Image.  index.html, registration.html has been changed.

--- src/main/java/com/example/springonlineshop/models/Category.java
+++ src/main/java/com/example/springonlineshop/models/Category.java
@@ -1,4 +1,19 @@
 package com.example.springonlineshop.models;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "category", fetch = FetchType.EAGER)
+    private List<Product> products;
+
+
 }
